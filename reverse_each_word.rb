d60f0ca728f24d95f2b356facf5a8d7b5a889bdb
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-18 16:09:06 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-def reverse_each_word(["Hello there, and how are you?"])
-  return reverse_each_word.each do |reverse_each_word|
+def reverse_each_word(p = ["Hello there, and how are you?"])
+  return reverse_each_word.each do |p|
   end
 end
